e631b4c7a292ff5795b9729acb2aae2253f8d7bf
Trey Young(goodpudding) <james.trey.young@gmail.com>
2023-03-08 15:26:18 -0800

    almost there

--- server.js
+++ server.js
@@ -15,27 +15,35 @@ app.get('/', (req, res) => {
   res.send('Hello from my server!');
 });
 
-app.get('/weather', (req, res) => {
+app.get('/weather', (req, res, next) => {
   let citySearched = req.query.city_name;
-  let getCity = data.find(search => search.city_name === citySearched);
-  let cityWeather = new Forecast(getCity);
-  console.log(getCity.data.datetime);
-  res.send(cityWeather);
+  try {
+  let getCity = data.find((search) => search.city_name === citySearched);
+    let cityWeather = new Forecast(getCity);
+    console.log(getCity.data.datetime);
+    res.send(cityWeather);
+  } catch (error) {
+    next(error);
+  }
 });
+
+app.use((error, request, response, next) => {
+  response.status(500).send(error.message);
+});
+
 
 app.get('*', (req, res) => {
   res.send('The resource does not exist');
 });
 
 class Forecast {
-  constructor(CityObject){
+  constructor(CityObject) {
     this.todaysDate = CityObject.data[0].valid_date;
     this.todaysDescription = CityObject.data[0].weather.description;
     this.tomorrowsDate = CityObject.data[1].valid_date;
     this.tomorrowsDescription = CityObject.data[1].weather.description;
     this.dayAfterTomDate = CityObject.data[2].valid_date;
     this.dayAfterTomDescription = CityObject.data[2].weather.description;
-
   }
 }
 
