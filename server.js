e07b3dbf6010b08cfa6be35610d8a28ffcf2d664
Trey Young(goodpudding) <james.trey.young@gmail.com>
2023-03-07 22:12:04 -0800

    card 2 complete

---
+++ server.js
@@ -0,0 +1,42 @@
+'use strict';
+
+console.log('My first server');
+
+const express = require('express');
+
+require('dotenv').config();
+
+let data = require('./data/weather.json');
+const app = express();
+
+const PORT = process.env.PORT || 3002;
+
+app.get('/', (req, res) => {
+  res.send('Hello from my server!');
+});
+
+app.get('/weather', (req, res) => {
+  let citySearched = req.query.city_name;
+  let getCity = data.find(search => search.city_name === citySearched);
+  let cityWeather = new Forecast(getCity);
+  console.log(getCity.data.datetime);
+  res.send(cityWeather);
+});
+
+app.get('*', (req, res) => {
+  res.send('The resource does not exist');
+});
+
+class Forecast {
+  constructor(CityObject){
+    this.todaysDate = CityObject.data[0].valid_date;
+    this.todaysDescription = CityObject.data[0].weather.description;
+    this.tomorrowsDate = CityObject.data[1].valid_date;
+    this.tomorrowsDescription = CityObject.data[1].weather.description;
+    this.dayAfterTomDate = CityObject.data[2].valid_date;
+    this.dayAfterTomDescription = CityObject.data[2].weather.description;
+
+  }
+}
+
+app.listen(PORT, () => console.log(`listening on port${PORT}`));
