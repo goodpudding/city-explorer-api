8ef71f8d230f0b10b5542a9774095ec4a0e67cfd
Trey Young(goodpudding) <james.trey.young@gmail.com>
2023-03-08 16:13:59 -0800

    fixed a few things

--- server.js
+++ server.js
@@ -18,10 +18,9 @@ app.get('/', (req, res) => {
 app.get('/weather', (req, res, next) => {
   let citySearched = req.query.city_name;
   try {
-  let getCity = data.find((search) => search.city_name === citySearched);
-    let cityWeather = new Forecast(getCity);
-    console.log(getCity.data.datetime);
-    res.send(cityWeather);
+    let getCity = data.find((search) => search.city_name === citySearched);
+    let selectedCity = getCity.data.map( passedObj => new Forecast(passedObj));
+    res.send(selectedCity);
   } catch (error) {
     next(error);
   }
@@ -38,12 +37,9 @@ app.get('*', (req, res) => {
 
 class Forecast {
   constructor(CityObject) {
-    this.todaysDate = CityObject.data[0].valid_date;
-    this.todaysDescription = CityObject.data[0].weather.description;
-    this.tomorrowsDate = CityObject.data[1].valid_date;
-    this.tomorrowsDescription = CityObject.data[1].weather.description;
-    this.dayAfterTomDate = CityObject.data[2].valid_date;
-    this.dayAfterTomDescription = CityObject.data[2].weather.description;
+    console.log(CityObject);
+    this.todaysDate = CityObject.valid_date;
+    this.todaysDescription = CityObject.weather.description;
   }
 }
 
